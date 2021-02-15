c4e0bc07a642c35470cd2c21b4f38f7ed0daa28d
Heather <machadoheather@gmail.com>
2021-02-15 14:58:11 +0000

    Updated create_input_files main

--- create_input_files/code_create_input_files.sh
+++ create_input_files/code_create_input_files.sh
@@ -14,7 +14,8 @@ chrom=3L
 chrom=3R
 
 ## Create RDataFile from vcf files (after Varscan SNP calling)
-Rscript make_melRDataFile.R mapping2015/RTECrun2/06_variant_calling/mel_$chrom\_mapping2016.varscanSNP_noindel_dpfilter_biallelic_repeatmask.freq.vcf mel_freqdp_$chrom\_042016.Rdata
+# vcf files available on datadryad: https://doi.org/10.5061/dryad.4r7b826
+Rscript make_melRDataFile.R mel_$chrom\_mapping2016.varscanSNP_noindel_dpfilter_biallelic_repeatmask.freq.vcf mel_freqdp_$chrom\_042016.Rdata
 
 ## Calculate Ne
 Rscript Ne.R mel_freqdp_$chrom\_042016.Rdata mel_nescent2_samplesize.txt mel_freqdp_$chrom\_042016_Ne.Rdata auto
@@ -48,3 +49,8 @@ java -Xmx4G -jar snpEff.jar dm5.48.reference mel_$chrom\_mapping2016.varscanSNP_
 ##  calculating Fst for samples that have a paired seasonal site (and climate data)
 Rscript make_mel_Fst_Ne.R ../data/mel_freqdp_042016_Ne_fixed.Rdata mel_Fst_Ne.seasonalpaired.txt
 Rscript make_mel_Fst_Ne_core20.R ../data/mel_freqdp_042016_Ne_fixed.Rdata $chrom ../data/mel_Fst_Ne.core20.sp_fall.txt
+
+## Creating sets of matched controls per SNP
+for chrom in 2L 2R 3L 3R; do
+    Rscript match_snps_dp_ch_SNPbySNP_recomb_chromposFilter.R ../data/means_dpfreq.$chrom.medfreq01_RRgrt0.recRate.Rdata bootstrap/bootstrap_fmean_dp.mel_$chrom.medfreq01_RRgrt0.recRate.polymorphic.txt 100 ../data/chrom_pos_polymorphic_medfreq01_RRgrt0.txt
+done
